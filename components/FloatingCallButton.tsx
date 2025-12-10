21866879bc6c67149ca5641fe58b80b18fa4990d
Braden <bbir1199@gmail.com>
2025-12-09 17:34:03 -0700

    this commit

--- components/FloatingCallButton.tsx
+++ components/FloatingCallButton.tsx
@@ -32,7 +32,7 @@ export default function FloatingCallButton() {
         </div>
       )}
 
-      <button
+      <button 
         className="
           fixed bottom-6 right-6
           w-16 h-16 rounded-full
